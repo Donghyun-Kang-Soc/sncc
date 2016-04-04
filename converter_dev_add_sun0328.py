06ae0c214a470a1c9d8f6c1cdfc7621831a6a348
eesun23 <eesun23@gmail.com>
2016-04-04 11:22:21 +0900

    Update converter_dev_add_sun0328.py

--- converter_dev_add_sun0328.py
+++ converter_dev_add_sun0328.py
@@ -8,9 +8,8 @@ Created on Mon Mar 28 15:54:51 2016
 
 class Sncc_converter:
     @staticmethod
-    def net_to_edgelist(input_f, output_f, sep = "\t"):
-        node_dict = {}
-        
+    def self_loop_deleter(input_f, output_f, sep = "\t"):
+    
         f = open(input_f, 'r')
         f_lines = f.readlines()
         edges_location = f_lines.index("*Edges\n")
@@ -33,11 +32,6 @@ class Sncc_converter:
         for line in edges_lines :
             splited_2 = line.split(edge_sep)
             
-            """
-        여기 아래 부분이 추가된 부분(line 42 ~ line 46):
-        self-loop 를 제외한 부분만 output_f 에 쓰도록
-        splited_2[0] != splited_2[1] 인 경우만 딕셔너리를 참조하여 값을 찾게함.            
-            """
             
             if str(splited_2[0]) != str(splited_2[1]).rstrip('\n'):
                 source = str(splited_2[0])
@@ -51,5 +45,5 @@ class Sncc_converter:
         out_put.close()
     
 if __name__ == "__main__":
-    Sncc_converter.net_to_edgelist('vignett37_network.net', 'test_37.txt')
-    Sncc_converter.edgelist_to_net()
+    Sncc_converter.self_loop_deleter('vignett37_network.net', 'test_37.txt')
+    Sncc_converter.self_loop_deleter()
